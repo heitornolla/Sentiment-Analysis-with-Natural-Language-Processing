ca909b830c17e7bfe4a858659d7b77190d8a501a
heitor <heitor.nolla@gmail.com>
2025-09-11 14:53:16 -0300

    add initial model code

---
+++ train.py
@@ -0,0 +1,89 @@
+import nltk
+import joblib
+import numpy as np
+import pandas as pd
+
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+def get_pt_stopwords():
+    try:
+        portuguese_stopwords = stopwords.words("portuguese")
+    except:
+        nltk.download("stopwords")
+        portuguese_stopwords = stopwords.words("portuguese")
+    return portuguese_stopwords
+
+
+def get_text_processing_pipeline(portuguese_stopwords):
+    pipeline = Pipeline(
+        [
+            (
+                "vectorizer",
+                TfidfVectorizer(
+                    max_features=300,
+                    min_df=7,
+                    max_df=0.8,
+                    stop_words=portuguese_stopwords,
+                    ngram_range=(1, 2),
+                ),
+            )
+        ]
+    )
+    return pipeline
+
+
+def get_model(pipeline, df_path="customer_reviews_preprocessed.csv"):
+    df = pd.read_csv(df_path)
+
+    if "comments" in df.columns:
+        X = df["comments"].astype(str).tolist()
+
+    y = df["sentiment"]
+
+    X_prep = pipeline.fit_transform(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_prep, y, test_size=0.20, random_state=42, stratify=y
+    )
+
+    logreg_param_grid = {
+        "C": np.linspace(0.1, 10, 20),
+        "penalty": ["l1", "l2"],
+        "class_weight": ["balanced", None],
+        "random_state": [42],
+        "solver": ["liblinear"],
+    }
+
+    logreg = LogisticRegression()
+    grid = GridSearchCV(
+        logreg, logreg_param_grid, cv=5, scoring="f1_macro", n_jobs=-1, verbose=1
+    )
+    grid.fit(X_train, y_train)
+
+    return grid, pipeline, (X_test, y_test)
+
+
+def predict_and_eval(grid, X_test, y_test):
+    y_pred = grid.predict(X_test)
+    print("Best Parameters:", grid.best_params_)
+    print("\nClassification Report:\n", classification_report(y_test, y_pred))
+    print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
+
+
+def save_model(grid, pipeline):
+    joblib.dump(grid.best_estimator_, "model/sentiment_model.pkl")
+    joblib.dump(pipeline, "model/tfidf_pipeline.pkl")
+
+
+if __name__ == "__main__":
+    portuguese_stopwords = get_pt_stopwords()
+    pipeline = get_text_processing_pipeline(portuguese_stopwords)
+    model, pipeline, (X_test, y_test) = get_model(pipeline)
+    predict_and_eval(model, X_test, y_test)
+    save_model(model, pipeline)
